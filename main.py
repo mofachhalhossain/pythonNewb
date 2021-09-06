465f1ebd1c04a46fb87c5d0f7067b015de6dbd90
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2021-09-06 22:40:01 +0600

    string deletion

--- main.py
+++ main.py
@@ -23,3 +23,8 @@ print("Sliced string: ", _str_access[2:5])
 #String Update
 _update_str_access = "Hello People"
 print("Updated String: "+_update_str_access)
+
+#String deletion
+'''del(_update_str_access)
+print(_update_str_access)'''
+'''del will delete entire string.'''
