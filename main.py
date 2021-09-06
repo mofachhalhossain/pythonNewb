f696ad31dbad7bf2ff7f1dea4fa0b1e9d820b28c
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2021-09-06 22:33:34 +0600

    accessing charecters

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ print(string2)
 string3 = '''I'm
     string3'''
 print(string3)
+
+#Accessing String Character
+_str_access = "Hello World"
+print("Initial String: ", _str_access)
+print("Accessing last character: "+_str_access[-1])
+print("Accessing first character: "+_str_access[0])
