7fd742645f849e22179679c060a9333ed37786fc
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2021-09-07 15:27:20 +0600

    list

--- main.py
+++ main.py
@@ -42,4 +42,48 @@ print("Formation in 3.2% format: ")
 print('Result: %3.2f' %integer)
 
 
+'''List'''
 
+#List creation
+
+_list1 = [1,2,3,'Hello','people','@','coding',44,'.com']
+print(_list1)
+
+#size or length of List
+print("Length of string:",len(_list1))
+
+#Adding Elements
+#Append
+_list1.append('Added Item')
+print(_list1)
+_list1.append(('Added Tuple1, Added Tuple2'))
+print(_list1)
+for i in range(4,8):
+    _list1.append(i)
+print(_list1)
+_list2 = ['Another','List']
+_list1.append(_list2)
+print(_list1)
+
+#Extend
+_list2.extend(['Extended','list'])
+print(_list2)
+
+#insert
+_list2.insert(4,'inserted')
+print(_list2)
+
+#Remove
+_list2.remove('inserted')
+print("removed",_list2)
+
+#even square
+_square_even = []
+for i in range(1,10):
+    if i%2!=1:
+        _square_even.append(i*i)
+print("Even square: ",_square_even)
+
+#Odd square
+_square_odd = [x*x for x in range(1,10) if x%2==1]
+print("Odd square: ", _square_odd)
